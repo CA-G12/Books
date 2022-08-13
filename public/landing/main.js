cf1e4447cbac126a40f446ca9b3f8013dfa9b7d2
Sami A Ba‘alousha <same-96@hotmail.com>
2022-08-13 12:49:32 +0300

    Delete main.js

--- public/landing/main.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-const searchInput = document.getElementById('searchiput');
-const searchSub = document.getElementById('searchSub');
-
-searchSub.addEventListener('click', (event) => {
-  event.preventDefault();
-  fetchSender('GET', `Books?q=${searchInput.value}`);
-  window.location.href = `Books?q=${searchInput.value}`;
-});
